d8d427ba67c93b38d6f44c48551f81ca24899357
PDC3A-Training.PDC3A <PDC3A-Training.PDC3A@accenture.com>
2018-01-31 16:14:21 +0530

    ddd

--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -23,7 +23,8 @@ public class HelloTest {
 		driver = new FirefoxDriver();*/
 		System.setProperty("webdriver.chrome.driver","resources\\chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.get("http://localhost:8087/Helloworld/");
+		driver.get("http://localhost:8087/HelloWorld/");
+
 		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
 	}
 
